fe84b1f01e4f54d9f834f431ee9fae684da288aa
BodanHarry <harry.bodan14945@est.uca.edu.ni>
2023-04-01 15:30:40 -0600

    spinner delete

--- contact.php
+++ contact.php
@@ -42,9 +42,7 @@ $email = isset($_POST["email"]);
 
 <body>
     <!-- Spinner Start -->
-    <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-        <div class="spinner"></div>
-    </div>
+    
     <!-- Spinner End -->
 
     <?php
