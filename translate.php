37c51cf815341f89a68941bc02fc9d1b4940ab28
davidquintr <davidquintr97@gmail.com>
2023-04-01 15:44:05 -0600

    listo asdljajskfjkdg

--- translate.php
+++ translate.php
@@ -92,24 +92,6 @@
    <a href="https://wa.me/15184958083" class="whatsapp" target="_blank"> <i class="fa fa-whatsapp whatsapp-icon"></i></a>
 </div>
 
-
-<!-- JS here -->
-<script src="assets/js/vendor/jquery-3.6.0.min.js"></script>
-<script src="assets/js/vendor/waypoints.min.js"></script>
-<script src="assets/js/bootstrap.bundle.min.js"></script>
-<script src="assets/js/meanmenu.js"></script>
-<script src="assets/js/swiper-bundle.min.js"></script>
-<script src="assets/js/owl.carousel.min.js"></script>
-<script src="assets/js/magnific-popup.min.js"></script>
-<script src="assets/js/parallax.min.js"></script>
-<script src="assets/js/backToTop.js"></script>
-<script src="assets/js/nice-select.min.js"></script>
-<script src="assets/js/counterup.min.js"></script>
-<script src="assets/js/ajax-form.js"></script>
-<script src="assets/js/wow.min.js"></script>
-<script src="assets/js/isotope.pkgd.min.js"></script>
-<script src="assets/js/imagesloaded.pkgd.min.js"></script>
-<script src="assets/js/main.js"></script>
 </body>
 
 </html>
